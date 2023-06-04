7f3613c17d08930a5b4406bcdaac2d82b4b657f1
Kimwel Beller <94892701+Kheemwel@users.noreply.github.com>
2023-06-04 12:20:58 +0800

    test download function

--- javascripts/function1.js
+++ javascripts/function1.js
@@ -1,3 +1,14 @@
 function showPrompt() {
     prompt("Enter your name:");
 }
+
+function downloadFileFromFolder() {
+    var filename = 'SciQuiz.apk'; // Replace with the actual path and filename of the file in your website folder
+    window.location.href = filename;
+}
+
+function downloadFileFromLink() {
+    const link = document.createElement('a');
+    link.href = 'https://github.com/Kheemwel/SciQuiz/releases/download/v1.1/SciQuiz.apk'; // Replace with the actual URL of the file
+    link.click();
+}
